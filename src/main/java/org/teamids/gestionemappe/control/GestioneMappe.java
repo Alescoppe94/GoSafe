56cb5f51f03461881253e71ade41b69540a01e57
DESKTOP-6LQKCIH\Alessandro <scoppe28994@gmail.com>
2018-03-02 11:14:01 +0100

    primo commit

---
+++ src/main/java/org/teamids/gestionemappe/control/GestioneMappe.java
@@ -0,0 +1,9 @@
+package org.teamids.gestionemappe.control;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("gestionemappe")
+public class GestioneMappe extends Application {
+
+}
