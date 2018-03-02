01e35c174a35b7022fd9491f062b98dece9c943a
lucalustrissimini <luca.lst@virgilio.it>
2018-03-02 18:39:08 +0100

    Aggiunte entity

---
+++ src/main/java/org/teamids/gestionemappe/model/Tappa.java
@@ -0,0 +1,23 @@
+package org.teamids.gestionemappe.model;
+
+public class Tappa {
+
+    private int id;
+    private Tronco tronco;
+
+    public Tappa(Tronco tronco) {
+        this.tronco = tronco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Tronco getTronco() {
+        return tronco;
+    }
+
+    public void setTronco(Tronco tronco) {
+        this.tronco = tronco;
+    }
+}
