0519261d29f4db348cd4f30d7996901d6d41c1db
Paolo <grossetti.paolo@libero.it>
2018-03-08 16:07:23 +0100

    Classi DAO e DbTable

---
+++ src/main/java/org/teamids/gestionemappe/model/DbTable/Corridoio.java
@@ -0,0 +1,11 @@
+package org.teamids.gestionemappe.model.DbTable;
+
+import org.teamids.gestionemappe.model.DbTableHelpers;
+
+public class Corridoio extends DbTableHelpers {
+
+    public Corridoio(){
+        name = "corridoio";
+        sql = "";
+    }
+}
