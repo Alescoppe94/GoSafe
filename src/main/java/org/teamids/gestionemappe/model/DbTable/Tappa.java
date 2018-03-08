7c20f4772b5dd1568d414c22e51ddfaa762fb183
lorenzobraconi <lorenzobraconi1994@gmail.com>
2018-03-08 16:09:48 +0100

    Aggiunti classi DAO e DbTable

---
+++ src/main/java/org/teamids/gestionemappe/model/DbTable/Tappa.java
@@ -0,0 +1,11 @@
+package org.teamids.gestionemappe.model.DbTable;
+
+import org.teamids.gestionemappe.model.DbTableHelpers;
+
+public class Tappa extends DbTableHelpers {
+
+    public Tappa(){
+        name = "tappa";
+        sql = "";
+    }
+}
