ad501eb6b609ff9bc27242493c9ce87d934936be
lorenzobraconi <lorenzobraconi1994@gmail.com>
2018-03-08 15:54:37 +0100

    Aggiunto package DbTable

---
+++ src/main/java/org/teamids/gestionemappe/model/DbTable/Utente.java
@@ -0,0 +1,11 @@
+package org.teamids.gestionemappe.model.DbTable;
+
+import org.teamids.gestionemappe.model.DbTable;
+
+public class Utente extends DbTable {
+
+    public Utente(){
+        name="utente";
+        sql="";
+    }
+}
