6e0c1aaaa35f632771ad5d3e1b8a7df54382b3a3
lucalustrissimini <luca.lst@virgilio.it>
2018-03-22 09:20:32 +0100

    Aggiunto spazio tra una riga

--- src/main/java/org/teamids/gestionemappe/boundary/GestioneMappeBoundary.java
+++ src/main/java/org/teamids/gestionemappe/boundary/GestioneMappeBoundary.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 public class GestioneMappeBoundary {
 
 
+
     @GET
     @Path("calcolapercorso/{beaconPart}/{beaconArr}")
     public String calcoloPercorso(@PathParam("beaconPart") int beaconPart,
