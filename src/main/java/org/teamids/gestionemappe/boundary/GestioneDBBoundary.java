c5ccdc65d16ba45565828d8e3535520df77b5da7
lorenzobraconi <lorenzobraconi1994@gmail.com>
2018-04-27 11:03:21 +0200

    sproato

--- src/main/java/org/teamids/gestionemappe/boundary/GestioneDBBoundary.java
+++ src/main/java/org/teamids/gestionemappe/boundary/GestioneDBBoundary.java
@@ -5,8 +5,11 @@ import org.teamids.gestionemappe.control.GestioneDB;
 import org.teamids.gestionemappe.model.DAO.BeaconDAO;
 import org.teamids.gestionemappe.model.entity.BeaconEntity;
 
+import javax.json.Json;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Path("/db")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,6 +21,13 @@ public class GestioneDBBoundary {
     public void aggiornaDB(@PathParam("beaconId") int id, BeaconEntity beacon){
         //beacon.setId(id);
         //BeaconDAO.updateBeacon(beacon);
+    }
+
+
+    @POST
+    @Path("/prova")
+    public void prova(javax.json.JsonObject requestBody) {
+        System.out.println(requestBody.get("hello"));
     }
 
   /*  @POST
