3d89abffaff08aaaba64ee7622b34d3654c16e7b
DESKTOP-5I1TOUH\Alessandro <scoppe28994@gmail.com>
2018-05-11 11:49:19 +0200

    supporto cors e aggiunta bozza metodo per admin

--- src/main/java/org/teamids/gestionemappe/boundary/GestioneDBBoundary.java
+++ src/main/java/org/teamids/gestionemappe/boundary/GestioneDBBoundary.java
@@ -1,9 +1,17 @@
 package org.teamids.gestionemappe.boundary;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.teamids.gestionemappe.control.GestioneDB;
+import org.teamids.gestionemappe.model.entity.PianoEntity;
 
+import javax.imageio.ImageIO;
+import javax.json.Json;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.sql.Timestamp;
 
 
@@ -18,5 +26,20 @@ public class GestioneDBBoundary {
     @Path("/aggiornadb/{timestamp}")
     public String aggiornaDB(@PathParam("timestamp") Timestamp timestamp){
         return gestionedb.aggiornaDB(timestamp);
+    }
+
+    @POST
+    @Path("/aggiungipiano")
+    public void aggiungiPiano(String piano) {
+        Gson pianojson = new Gson();
+        JsonObject jsonRequest = pianojson.fromJson(piano, JsonObject.class);
+        /*String base64Image = piano.getImmagine().split(",")[1];
+        byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(base64Image);
+        try {
+            BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        System.out.println(piano);
     }
 }
